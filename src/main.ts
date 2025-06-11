5eabe3cdc539e3154358a1e7f8860800937090cc
oLo <>
2025-06-11 22:01:18 +0200

    shrink the bangs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,42 @@
 import bangs from "./bangs.min.json";
 import "./global.css";
 
-function noSearchDefaultPageRender() {
+const url = new URL(window.location.href);
+const query = url.searchParams.get("q")?.trim() ?? "";
+if (query) doBangRedirect(query);
+else renderUI();
+
+const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
+const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
+
+function doBangRedirect(query: string) {
+  const match = query.match(/!(\S+)/i);
+
+  const bangCandidate = match?.[1]?.toLowerCase();
+  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+
+  // Remove the first bang from the query
+  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
+
+  // If the query is just `!gh`, use `github.com` instead of `github.com/search?q=`
+  if (cleanQuery === "") {
+    return selectedBang ? new URL(selectedBang.u).origin : undefined;
+  }
+
+  // Format of the url is:
+  // https://www.google.com/search?q={{{s}}}
+  const searchUrl = selectedBang?.u.replace(
+    "{{{s}}}",
+    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+    encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
+  );
+
+  if (!searchUrl) return;
+
+  window.location.replace(searchUrl);
+}
+
+function renderUI() {
   const app = document.querySelector<HTMLDivElement>("#app")!;
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
@@ -43,46 +78,3 @@ function noSearchDefaultPageRender() {
     }, 2000);
   });
 }
-
-const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
-const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
-
-function getBangredirectUrl() {
-  const url = new URL(window.location.href);
-  const query = url.searchParams.get("q")?.trim() ?? "";
-  if (!query) {
-    noSearchDefaultPageRender();
-    return null;
-  }
-
-  const match = query.match(/!(\S+)/i);
-
-  const bangCandidate = match?.[1]?.toLowerCase();
-  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
-
-  // Remove the first bang from the query
-  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
-
-  // If the query is just `!gh`, use `github.com` instead of `github.com/search?q=`
-  if (cleanQuery === "")
-    return selectedBang ? `https://${selectedBang.d}` : null;
-
-  // Format of the url is:
-  // https://www.google.com/search?q={{{s}}}
-  const searchUrl = selectedBang?.u.replace(
-    "{{{s}}}",
-    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
-    encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
-  );
-  if (!searchUrl) return null;
-
-  return searchUrl;
-}
-
-function doRedirect() {
-  const searchUrl = getBangredirectUrl();
-  if (!searchUrl) return;
-  window.location.replace(searchUrl);
-}
-
-doRedirect();
