070920d1703c872521bc21ac4781174142c632a5
oLo <>
2025-06-13 02:43:42 +0200

    refactor main / use KV for bangs

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,111 @@
 import bangs from "./bangs.min.json";
 import "./global.css";
 
-const url = new URL(window.location.href);
-const query = url.searchParams.get("q")?.trim() ?? "";
-if (query) doBangRedirect(query);
-else renderUI();
+type Trigger = keyof typeof bangs;
 
-const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
-const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
+const DEFAULT_BANG = "g";
 
-function doBangRedirect(query: string) {
-  const match = query.match(/!(\S+)/i);
-
-  const bangCandidate = match?.[1]?.toLowerCase();
-  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
-
-  // Remove the first bang from the query
-  const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
-
-  // If the query is just `!gh`, use `github.com` instead of `github.com/search?q=`
-  if (cleanQuery === "") {
-    return selectedBang ? new URL(selectedBang.u).origin : undefined;
+class NoBangError extends Error {
+  constructor() {
+    super(`no bang provided, applying defaults`);
+    this.name = "NoBangError";
   }
-
-  // Format of the url is:
-  // https://www.google.com/search?q={{{s}}}
-  const searchUrl = selectedBang?.u.replace(
-    "{{{s}}}",
-    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
-    encodeURIComponent(cleanQuery).replace(/%2F/g, "/"),
-  );
-
-  if (!searchUrl) return;
-
-  window.location.replace(searchUrl);
 }
 
-function renderUI() {
-  const app = document.querySelector<HTMLDivElement>("#app")!;
-  app.innerHTML = `
-    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
-      <div class="content-container">
-        <h1>Und*ck</h1>
-        <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
-        <div class="url-container"> 
-          <input 
-            type="text" 
-            class="url-input"
-            value="https://unduck.link?q=%s"
-            readonly 
-          />
-          <button class="copy-button">
-            <img src="/clipboard.svg" alt="Copy" />
-          </button>
-        </div>
-      </div>
-      <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
-        •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
-      </footer>
-    </div>
-  `;
-
-  const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
-  const copyIcon = copyButton.querySelector("img")!;
-  const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
-
-  copyButton.addEventListener("click", async () => {
-    await navigator.clipboard.writeText(urlInput.value);
-    copyIcon.src = "/clipboard-check.svg";
-
-    setTimeout(() => {
-      copyIcon.src = "/clipboard.svg";
-    }, 2000);
-  });
+class InvalidBangError extends Error {
+  constructor(bang: string) {
+    super(`bang "${bang}" is not valid`);
+    this.name = "InvalidBangError";
+  }
 }
+
+async function redirect() {
+  const url = new URL(location.href);
+  let query = url.searchParams.get("q")?.trim() ?? "";
+  let redirectUrl: string = "";
+
+  try {
+    if (!query) throw new NoBangError();
+
+    // cut trigger out from query
+    const queryTrigger = query.match(/!(\S+)/i)?.[1]?.toLowerCase();
+    query = query.replace(/!\S+\s*/i, "").trim();
+
+    if (queryTrigger) {
+      redirectUrl = bangs[queryTrigger as Trigger];
+      if (redirectUrl) return;
+    }
+
+    // user didn't typed an invalid bang, apply a default where `default <- url hash ?? local-storage ?? DEFAULT_BANG`
+
+    const local = localStorage.getItem("default-bang");
+    if (local) {
+      // 1. try bang from localStorage, throws if trigger is invalid
+      console.warn(`bang not found, using to local storage bang "${local}"`);
+
+      redirectUrl = bangs[local as Trigger];
+      if (!redirectUrl) throw new InvalidBangError(local);
+    }
+
+    const hash = url.hash.slice(1).trim() ?? "";
+    if (hash) {
+      // 2. try bang from url hash, throws if trigger is invalid
+      console.warn(`bang not found. using url hash bang "${hash}"`);
+
+      redirectUrl = bangs[hash as Trigger];
+      if (!redirectUrl) throw new InvalidBangError(hash);
+    }
+
+    // 3. use hard-coded default bang
+    console.warn(`bang not found. using default bang "${DEFAULT_BANG}"`);
+    redirectUrl = bangs[DEFAULT_BANG];
+  } catch (err) {
+    // TODO error handling - look up wix guidelines for error handling
+    // https://wix-ux.com/when-life-gives-you-lemons-write-better-error-messages-46c5223e1a2f
+    // https://miro.medium.com/v2/resize:fit:720/format:webp/1*lbm-FjstINfLqXVrVMgonA.png
+
+    switch (true) {
+      case err instanceof NoBangError: {
+        // TODO: render '/' route
+        const { renderUI } = await import("./ui.js");
+        renderUI();
+        return;
+      }
+
+      case err instanceof InvalidBangError: {
+        // TODO:
+        // - render '/customize' route
+        // - display error message "bang was not found, suggest user edits their bangs
+        const { renderUI } = await import("./ui.js");
+        renderUI();
+        return;
+      }
+
+      default: {
+        console.error("Unexpected error:", err);
+        return;
+      }
+    }
+  } finally {
+    // If the query is just `!gh`, use `https://github.com` instead of `https://github.com/search?q=`
+    if (query === "") {
+      redirectUrl = new URL(redirectUrl).origin;
+    } else {
+      redirectUrl = redirectUrl.replace(
+        // Format of the url is: https://www.google.com/search?q={{{s}}}
+        "{{{s}}}",
+        // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+        encodeURIComponent(query).replace(/%2F/g, "/"),
+      );
+    }
+
+    // add https:// if not present
+    if (!redirectUrl.startsWith("http")) {
+      redirectUrl = `https://${redirectUrl}`;
+    }
+
+    window.location.replace(redirectUrl);
+  }
+}
+
+await redirect();
